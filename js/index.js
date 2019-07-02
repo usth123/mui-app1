ef8506feda8b1059f003410e15ce09779e817f0a
sth <usth05@163.com>
2019-07-02 13:50:50 +0800

    复制

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function jsCopy(obj) {
+/*function jsCopy(obj) {
     var u = navigator.userAgent;
     console.log(u)
     //苹果 
@@ -39,4 +39,66 @@ function jsCopy(obj) {
         alert('若点击复制文案无效，请长按内容手动复制！');
     }
     $('#isShow').html("复制成功");
-}
+}*/
+
+// 复制文案功能:
+const jsCopy = function(obj) {
+
+    function fn() {
+        var u = navigator.userAgent;
+        //苹果
+        if (u.match(/(iPhone|iPod|iPad);?/i)) { //ios
+            //   alert('苹果啊');
+            //要复制文字的节点
+            var copyDOM = obj.parentNode.firstChild.nextElementSibling;
+
+            var range = document.createRange();
+            // 选中需要复制的节点
+            range.selectNode(copyDOM);
+            // 执行选中元素
+            window.getSelection().addRange(range);
+            // 执行 copy 操作
+            var successful = document.execCommand('copy');
+            try {
+                var msg = successful ? 'successful' : 'unsuccessful';
+                console.log('copy is' + msg);
+            } catch (err) {
+                console.log('Oops, unable to copy');
+            }
+            // 移除选中的元素
+            window.getSelection().removeAllRanges();
+            obj.innerHTML = '已复制'
+
+
+            //obj.setAttribute('data-state','yes');
+
+        }
+        // 安卓
+        if (u.indexOf('Android') > -1) {
+            // alert('安卓啊');
+            var $copyPP = obj.parentNode.firstChild.nextElementSibling,
+                $copyText = obj.parentNode.lastChild.nextElementSibling;
+            $copyText.innerHTML = $copyPP.innerHTML.replace(/<[^>]+>/g, "");
+
+            $copyText.select(); // 选择对象
+
+            document.execCommand("Copy"); // 执行浏览器复制命令
+
+            obj.innerHTML = '已复制';
+        }
+        // 安卓系统的UC浏览器
+        if (u.indexOf('Android') > -1 && u.indexOf('UCBrowser') > -1) {
+            obj.innerHTML = '点击复制文案';
+            alert('若点击复制文案无效，请长按内容手动复制！');
+        }
+    }
+
+    obj.addEventListener('click', fn, false);
+};
+
+//
+let copyBtns = document.querySelectorAll('.js-copy-btn');
+copyBtns.forEach((obj) => {
+    jsCopy(obj);
+
+});
